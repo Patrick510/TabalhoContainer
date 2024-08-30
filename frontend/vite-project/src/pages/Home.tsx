70be8aecc2488d5e45122d3209d5bd6e1ea4c18b
Patrick Dias <117788162+Patrick510@users.noreply.github.com>
2024-08-30 13:58:14 -0400

    chore: Initialize `produtos` state with empty array in Home component
    The code changes initialize the `produtos` state with an empty array in the Home component. This ensures that the state is properly initialized and prevents any potential errors when rendering the list of products.

--- frontend/vite-project/src/pages/Home.tsx
+++ frontend/vite-project/src/pages/Home.tsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import { Button } from "../components/ui/button";
 import { useFetchProd } from "../components/hooks/useFetchProd";
 
+interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+}
+
 export default function Home() {
-  const [produtos, setProdutos] = useState([]);
+  const [produtos, setProdutos] = useState([] as Produto[]);
   const [contador, setContador] = useState(0);
 
-  const {
-    data: [],
-  } = useFetchProd();
+  const { data } = useFetchProd();
+
+  if (data && produtos.length === 0) {
+    setProdutos(data);
+  }
 
   function incrementar() {
     setContador(contador + 1);
@@ -26,7 +34,7 @@ export default function Home() {
       <div className="criap bg-slate-600"></div>
 
       <div>
-        {data?.map((produto) => (
+        {produtos.map((produto) => (
           <div key={produto.id}>
             <p>{produto.nome}</p>
             <p>{produto.preco}</p>
