ef8e33e44547e98cf59cbb9215570fd2d4382012
Patrick Dias <117788162+Patrick510@users.noreply.github.com>
2024-08-30 13:55:28 -0400

    chore: Update HTML title and favicon
    ```

---
+++ frontend/vite-project/src/components/hooks/useFetchProd.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export const useFetchProd = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`http://localhost:8000/api/getProd`);
+        const json = await res.json();
+        setData(json);
+
+        setLoading(false);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError("Ocorreu um erro desconhecido");
+        }
+        console.error("Não foi possível encontrar os dados desse CEP");
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
---
+++ frontend/vite-project/src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Button } from "../components/ui/button";
+import { useFetchProd } from "../components/hooks/useFetchProd";
+
+export default function Home() {
+  const [produtos, setProdutos] = useState([]);
+  const [contador, setContador] = useState(0);
+
+  const { data } = useFetchProd();
+
+  function incrementar() {
+    setContador(contador + 1);
+    const p = document.createElement("p");
+    p.innerHTML = `Contador: ${contador}`;
+    document.querySelector(".criap")?.appendChild(p);
+  }
+
+  return (
+    <div className="bg-black">
+      <p>deu bom</p>
+      <Button variant={"outline"} onClick={() => incrementar()}>
+        ADICIONAR CONTADOR
+      </Button>
+      <div className="criap bg-slate-600"></div>
+
+      <div>
+        {data &&
+          data.map((produto) => (
+            <div key={produto.id}>
+              <p>{produto.nome}</p>
+              <p>{produto.preco}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
