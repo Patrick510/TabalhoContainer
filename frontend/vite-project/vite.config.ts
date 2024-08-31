47471fe762ec48a12b92cfda79a6b5c7e2addc64
Patrick Dias <117788162+Patrick510@users.noreply.github.com>
2024-08-30 21:10:13 -0400

    chore: Update index.html and vite.config.ts
    This commit updates the `index.html` file by changing the page title to "Controle Produtos". Additionally, the `vite.config.ts` file is modified to add a proxy configuration for API requests. The proxy redirects requests starting with "/api" to the local server at "http://localhost:8000". This change improves the functionality and integration of the frontend with the backend API.

--- frontend/vite-project/vite.config.ts
+++ frontend/vite-project/vite.config.ts
@@ -14,5 +14,12 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
